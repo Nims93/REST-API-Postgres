348a857fa7a0c3ddd168e948af73458df77e10f3
Kieran C <kieranakc2@gmail.com>
2021-11-30 21:31:38 +0000

    changed folder structure

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
 const express = require('express');
+const db = require('./db');
 const app = express();
-
-const PORT = process.env.SERVER_PORT || 3001;
+const todosRouter = require('./routes/todos');
+const PORT = process.env.NODE_SERVER_PORT || 3001;
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('API working!');
 });
+
+app.use('/todos', todosRouter);
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
