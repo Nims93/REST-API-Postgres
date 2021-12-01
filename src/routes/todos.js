7be78209887d7e5f75d6e344e845f5f7ff325cc0
Kieran C <kieranakc2@gmail.com>
2021-12-01 15:20:37 +0000

    added POST /todos route add todo to db

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -14,4 +14,25 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    if (!title || !content) {
+      throw new Error('One or more paramaters are missing');
+    }
+
+    const data = await pool.query(
+      `INSERT INTO todos (title, content) 
+      VALUES ($1, $2) returning *`,
+      [title, content]
+    );
+    res.status(201).json({ success: true, added: data.rows[0] });
+  } catch (err) {
+    if (err.message === 'One or more paramaters are missing') {
+      console.error(`Error: ${err.message} @ POST route: /todos`);
+      res.status(400).json({ success: false, message: err.message });
+    }
+  }
+});
+
 module.exports = router;
