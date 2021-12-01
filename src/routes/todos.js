a77254d306a1ca527e9f43657c688db4bf5c1b4d
Kieran C <kieranakc2@gmail.com>
2021-12-01 16:39:03 +0000

    added PUT todo/:id route update todo contents

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -1,3 +1,4 @@
+const { json } = require('express');
 const express = require('express');
 const router = express.Router();
 const pool = require('../db');
@@ -26,8 +27,48 @@ router.get('/:id', async (req, res) => {
     res.json({ success: true, data: data.rows[0] });
   } catch (err) {
     if (err.message === 'ID not found')
-      res.status(404).json({ success: false });
+      res.status(404).json({ success: false, message: err.message });
     console.error(err.message);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const {
+      body: { title, content },
+      params: { id },
+    } = req;
+
+    let args;
+    let query;
+
+    if (!title && !content) throw new Error('No data supplied');
+    else if (!title || !content) {
+      query = `UPDATE todos SET 
+      title=${title ? '$1' : 'title'}, 
+      content=${content ? '$1' : 'content'} 
+      WHERE id=$2 returning *`;
+      args = [title ? title : content, parseInt(id)];
+    } else {
+      query = `UPDATE todos SET 
+      title=$1, content= $2 
+      WHERE id=$3 returning *`;
+      args = [title, content, id];
+    }
+
+    const data = await pool.query(query, args);
+
+    if (!data.rows.length) throw new Error('ID not found');
+
+    res.json({ success: true, data: data.rows[0] });
+  } catch (err) {
+    if (err.message === 'No data supplied') {
+      res.status(400).json({ success: false, message: err.message });
+    } else if (err.message === 'ID not found') {
+      res.status(404).json({ success: false, message: err.message });
+    }
+    console.error(err.message);
+    // res.status(400).json({ success: false, message: err.message });
   }
 });
 
