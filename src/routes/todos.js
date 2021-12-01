eabe377b0413f907a17f480ec59e5a5f3e210fc8
Kieran C <kieranakc2@gmail.com>
2021-12-01 15:17:58 +0000

    GET /todos route added

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../db');
 
-router.get('/', (req, res) => {
-  res.json({ message: 'route working' });
+//route /todos/
+
+router.get('/', async (req, res) => {
+  try {
+    const data = await pool.query(`SELECT * FROM todos`);
+    res.json(data.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(400).json({ success: false, message: 'Something went wrong' });
+  }
 });
 
 module.exports = router;
