892ce60c280c1aa0fffc351213cb3803f97ccddd
Kieran C <kieranakc2@gmail.com>
2021-12-04 19:11:17 +0000

    todos delete route testing

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,5 +1,6 @@
 const makeApp = require('./../app');
 const request = require('supertest');
+const { response } = require('express');
 // const { jest } = require('@jest/globals');
 
 const mockDatabase = {
@@ -18,6 +19,22 @@ const mockDatabase = {
       ? { rows: [{ id: parseInt(id), title: 'test', content: 'test' }] }
       : { rows: [] }
   ),
+
+  putTodoID: jest.fn((id, title, content) => {
+    if (id < 1) {
+      return { rows: [] };
+    } else if (!title || !content) {
+      return {
+        rows: [
+          {
+            id: parseInt(id),
+            title: title ? title : 'original database value',
+            content: content ? content : 'original database value',
+          },
+        ],
+      };
+    }
+  }),
 };
 
 const app = makeApp(mockDatabase);
@@ -80,20 +97,55 @@ describe('POST /todos', () => {
 describe('DELETE /todos', () => {
   afterEach(() => mockDatabase.deleteTodo.mockClear());
 
-  const i = 1;
   test('should respond with json data if id is valid', async () => {
-    await request(app)
-      .delete(`/todos/${i}`)
-      .expect(200, {
-        success: true,
-        data: { id: i, title: 'test', content: 'test' },
-      });
+    for (let i = 1; i < 4; i++) {
+      await request(app)
+        .delete(`/todos/${i}`)
+        .expect(200, {
+          success: true,
+          data: { id: i, title: 'test', content: 'test' },
+        });
+    }
   });
 
   test('should repsond with 404 error if id param is invalid', async () => {
     await request(app)
-      .delete(`/todos/${0}`)
+      .delete('/todos/0')
       .expect('Content-Type', /json/)
       .expect(400);
+  });
+});
+
+describe('PUT /todos/:id route', () => {
+  afterEach(() => mockDatabase.putTodoID.mockClear());
+
+  const sendData = [
+    {},
+    { title: 'test title' },
+    { content: 'test content' },
+    { ttile: 'test title', content: 'test content' },
+  ];
+
+  test('should respond with json data if id is in db', async () => {
+    for (let i = 1; i < 4; i++) {
+      mockDatabase.putTodoID.mockClear();
+
+      await request(app)
+        .put(`/todos/${i}`)
+        .send(sendData[i])
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(mockDatabase.putTodoID.mock.calls.length).toBe(1);
+    }
+  });
+
+  test('should respond with 404 if id not in db', async () => {
+    await request(app)
+      .put('/todos/0')
+      .send({ title: 'test', content: 'test' })
+      .expect(404);
+
+    expect(mockDatabase.putTodoID.mock.calls.length).toBe(1);
   });
 });
